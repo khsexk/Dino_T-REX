873206e512419e9241b23e55d17c99794913d891
khsexk <kohyunsuk98@gmail.com>
2021-11-18 02:43:46 +0900

    크롬 공룡 게임

---
+++ JSP Project/src/main/webapp/gameFolder/gameJS.js
@@ -0,0 +1,65 @@
+let container = document.querySelector("#container");
+let dino = document.querySelector("#dino");
+let block = document.querySelector("#block");
+let road = document.querySelector("#road");
+let cloud = document.querySelector("#cloud");
+let score = document.querySelector("#score");
+let gameOver = document.querySelector("#gameOver");
+
+let interval = null;
+let playerScore = 0;
+
+// score 구현
+let scoreCounter = ()=>{
+    playerScore++;
+    score.innerHTML = `Score <b>${playerScore}</b>`;
+}
+
+// 게임 시작
+window.addEventListener("keydown", (start)=>{
+    console.log(start);
+    if(start.code == "Space"){
+        gameOver.style.display = "none";
+        block.classList.add("blockActive");
+        road.firstElementChild.style.animation = "roadAnimate 1.5s linear infinite";
+        cloud.firstElementChild.style.animation = "cloudAnimate 50s linear infinite";
+
+        // score
+        playerScore = 0;
+        interval = setInterval(scoreCounter, 60);
+    }
+})
+
+// 점프
+window.addEventListener("keydown", (e)=>{
+    console.log(e);
+
+    if(e.key=="ArrowUp"){
+        if(dino.classList != "dinoActive"){
+            dino.classList.add("dinoActive");
+            
+            setTimeout(()=>{
+                dino.classList.remove("dinoActive");
+            }, 500);
+        }
+    }
+})
+
+// 충돌 → 게임오버
+let result = setInterval(()=>{
+    let dinoBottom = parseInt(getComputedStyle(dino).getPropertyValue("bottom"));
+    console.log("DinoBottom" + dinoBottom);
+
+    let blockLeft = parseInt(getComputedStyle(block).getPropertyValue("left"));
+    console.log("BlockLeft" + blockLeft);
+
+    if(dinoBottom <= 90 && blockLeft >= 20 && blockLeft <= 145){
+        console.log("Game Over");
+
+        gameOver.style.display = "block";
+        block.classList.remove("blockActive");
+        road.firstElementChild.style.animation = "none";
+        cloud.firstElementChild.style.animation = "none";
+        clearInterval(interval);
+    }
+}, 10);
