9b206d8a69c71626c0c3f89fb6c1264b98a81a61
khsexk <kohyunsuk98@gmail.com>
2021-11-18 03:32:10 +0900

    회원가입 및 로그인 기본 틀 구성

---
+++ JSP Project/src/main/java/member/Domain/memberVO.java
@@ -0,0 +1,54 @@
+package member.Domain;
+
+public class memberVO {
+	private String id;
+	private String passwd;
+	private String username;
+	private String nation;
+	private String old;
+	private String mobile;
+	private String email;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPasswd() {
+		return passwd;
+	}
+	public void setPasswd(String passwd) {
+		this.passwd = passwd;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getNation() {
+		return nation;
+	}
+	public void setNation(String nation) {
+		this.nation = nation;
+	}
+	public String getOld() {
+		return old;
+	}
+	public void setOld(String old) {
+		this.old = old;
+	}
+	public String getMobile() {
+		return mobile;
+	}
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
